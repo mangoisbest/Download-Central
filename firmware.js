23e03caa44c215801693b4d36a6cad18b7c5ab2a
FB4 STUDIOS <69620839+mangoisbest@users.noreply.github.com>
2022-11-24 15:22:19 +1000

    big update

--- firmware.js
+++ firmware.js
@@ -1,3 +1,13 @@
-function MakeApp(title) {
-  document.getElementById('appTitle').innerHTML = title;
-}
+document.getElementById("BotzStoreButton").addEventListener("click", BotzStore)
+document.getElementById("OperaGXButton").addEventListener("click", OpreaGX)
+
+function BotzStore() {
+  appTitle.innerHTML = "App Name: Botz Store"
+  appCreator.innerHTML = "App Creator: ASHES SOFTWARE"
+};
+
+function OpreaGX() {
+  appTitle.innerHTML = "App Name: Oprea GX Browser"
+  appCreator.innerHTML = "App Creator: n/a"
+};
+
